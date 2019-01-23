225c7527d23e3f80ec6b399e33af10c57a7ff7aa
Tomasz Rogalski <tomracc@gmail.com>
2019-01-23 22:58:51 +0100

    Draft continued

--- js/app.js
+++ js/app.js
@@ -1,24 +1,39 @@
 $(function() {
 
-
-
     var displayErrorMessage = function(){
         var errorDiv = $("<div>");
         $("body").append(errorDiv);
         errorDiv.text("Unable to load the records");
     }
 
-
     $.get("http://localhost:8282/books/").done(function(result) {
         
-        var table=$("table");
+        var body=$("body");
 
         for(var i=0; i<result.length; i++){
-            table.append(`<tr><td>${result[i].title}</td></tr>`);
+            var element = $(`<p id=${result[i].id}>${result[i].title}</p><div></div>`)
+            body.append(element);
         };
 
+        $("p").on("click",function(){
+            var bookId=$(this).attr("id");
+
+            $.get(`http://localhost:8282/books/${bookId}`).done(function(book){
+                var info = `${book.isbn}`;
+                var target = $(`#${book.id}`).next();
+                if(target.is(':empty')){
+                    target.append(info);
+                } else {
+                    target.empty();
+                }
+               
+            })
+        });
+
+        // isbn	"9788324631766"
+        // title	"Thiniking in Java"
+        // author	"Bruce Eckel"
+        // publisher	"Helion"
+        // type	"programming"
     }).fail(displayErrorMessage);
-
-
-
 });
