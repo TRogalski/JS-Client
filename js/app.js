0ba2f9aeaee2ff9609893ad1bac62e65338b8848
TRogalski <tomracc@gmail.com>
2019-01-26 20:18:41 +0100

    added POST

--- js/app.js
+++ js/app.js
@@ -1,23 +1,13 @@
 $(function() {
 
-    var displayErrorMessage = function(){
-        var errorDiv = $("<div>");
-        $("body").append(errorDiv);
-        errorDiv.text("Unable to load the records");
-    }
 
-    $.get("http://localhost:8282/books/").done(function(result) {
-        
+    function addBook(book){
         var body=$("body");
-
-        for(var i=0; i<result.length; i++){
-            var element = $(`<p id=${result[i].id}>${result[i].title}</p><div></div>`)
-            body.append(element);
-        };
-
-        $("p").on("click",function(){
-            var bookId=$(this).attr("id");
-
+        var element = $(`<p id=${book.id}>${book.title}</p><div></div>`);
+        body.append(element);
+        $(`#${book.id}`).on("click", function(book){
+            
+            var bookId = $(this).attr("id");
             $.get(`http://localhost:8282/books/${bookId}`).done(function(book){
                 var info = `isbn: ${book.isbn}<br> author: ${book.author}<br> publisher: ${book.publisher} <br> type: ${book.type}`;
                 var target = $(`#${book.id}`).next();
@@ -26,14 +16,34 @@ $(function() {
                 } else {
                     target.empty();
                 }
-               
             })
-        });
+        })
+    }
 
-        // isbn	"9788324631766"
-        // title	"Thiniking in Java"
-        // author	"Bruce Eckel"
-        // publisher	"Helion"
-        // type	"programming"
-    }).fail(displayErrorMessage);
+    $.get(`http://localhost:8282/books/`).done(function(books) {
+        for(var book of books){
+            addBook(book);
+        }
+    })
+
+    $("#test-form").submit(function(e){
+        e.preventDefault();
+        
+        var isbn = $("#test-form input[name=isbn]").val();
+        var title = $("#test-form input[name=title]").val();
+        var author = $("#test-form input[name=author]").val();
+        var publisher = $("#test-form input[name=publisher]").val();
+        var type = $("#test-form input[name=type]").val();
+
+        $.ajax({
+            url: 'http://localhost:8282/books',
+            type: 'POST',
+            data: JSON.stringify({isbn, title, author, publisher, type}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).done(function(book){
+            addBook(book);
+        })
+    })
 });
